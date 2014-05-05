861769346094d8c73210a095e16fc30022334e58
Whitney Young <wbyoung@fadingred.com>
2014-05-04 22:26:46 -0700

    More comprehensive tests for hooks.

---
+++ test/hooks.js
@@ -0,0 +1,182 @@
+/* jshint expr: true */
+/* global before, beforeEach, after */
+
+var avn = require('..');
+var path = require('path');
+var chalk = require('chalk');
+var plugins = require('../lib/plugins');
+var concat = require('concat-stream');
+
+var sinon = require('sinon');
+var chai = require('chai');
+var expect = chai.expect;
+chai.use(require('sinon-chai'));
+
+var capture = require('./helpers').capture;
+var example = function(name) {
+  return path.join(__dirname, 'examples', name);
+};
+
+describe('avn', function() {
+
+  describe('hooks', function() {
+    var all = plugins.all;
+    var chalkEnabled = chalk.enabled;
+    var plugin;
+
+    before(function() {
+      chalk.enabled = false;
+      plugins.all = function() {
+        return [plugin];
+      };
+    });
+
+    after(function() {
+      chalk.enabled = chalkEnabled;
+      plugins.all = all;
+    });
+
+    beforeEach(function() {
+      plugin = {
+        name: 'test',
+        match: sinon.spy(function(v) {
+          return { version: 'v' + v, command: 'node-version-tool activate ' + v };
+        })
+      };
+    });
+
+    describe('before', function() {
+      it('exists', function(done) {
+        avn.hooks.before(example('v0.10.26')).done(done);
+      });
+    });
+
+    describe('after', function() {
+      it('does nothing when no version file exists', function(done) {
+        var std = capture();
+        avn.hooks.after(example('none')).done(function() {
+          std.restore();
+          expect(plugin.match).to.not.have.been.called;
+          expect(std.err).to.be.empty;
+          expect(std.out).to.be.empty;
+          expect(std.cmd).to.be.empty;
+          done();
+        });
+      });
+
+      it('calls plugin match function', function(done) {
+        var std = capture();
+        avn.hooks.after(example('v0.10.26')).done(function() {
+          std.restore();
+          expect(plugin.match).to.have.been.calledWith('0.10.26');
+          expect(std.err).to.be.empty;
+          expect(std.out).to.eql('avn using node 0.10.26 (test v0.10.26)\n');
+          expect(std.cmd).to.eql('node-version-tool activate 0.10.26\n');
+          done();
+        });
+      });
+
+      it('fails if plugin returns undefined', function(done) {
+        var std = capture();
+        plugin = { name: 'test', match: function() {} };
+        avn.hooks.after(example('v0.10.26')).done(function() {
+          std.restore();
+          expect(std.out).to.be.empty;
+          expect(std.cmd).to.be.empty;
+          expect(std.err).to.eql('avn could not activate node 0.10.26\n');
+          done();
+        });
+      });
+
+      it('fails when plugin throws', function(done) {
+        var std = capture();
+        plugin = {
+          name: 'test',
+          match: function() { throw new Error('test'); }
+        };
+        avn.hooks.after(example('v0.10.26')).done(function() {
+          std.restore();
+          expect(std.out).to.be.empty;
+          expect(std.cmd).to.be.empty;
+          expect(std.err).to.eql('avn could not activate node 0.10.26\n');
+          done();
+        });
+      });
+
+      it('handles plugin throwing errors', function(done) {
+        var std = capture();
+        plugin = {
+          name: 'test',
+          match: function() { throw new Error('test'); }
+        };
+        avn.hooks.after(example('v0.10.26'), { verbose: true }).done(function() {
+          std.restore();
+          expect(std.out).to.be.empty;
+          expect(std.cmd).to.be.empty;
+          expect(std.err).to.eql('avn could not activate node 0.10.26\n' +
+            'error: no plugin passed predicate\n  test: test\n');
+          done();
+        });
+      });
+
+      it('handles plugin throwing strings', function(done) {
+        var std = capture();
+        plugin = {
+          name: 'test',
+          match: function() { throw 'test'; }
+        };
+        avn.hooks.after(example('v0.10.26'), { verbose: true }).done(function() {
+          std.restore();
+          expect(std.out).to.be.empty;
+          expect(std.cmd).to.be.empty;
+          expect(std.err).to.eql('avn could not activate node 0.10.26\n' +
+            'error: no plugin passed predicate\n  test: test\n');
+          done();
+        });
+      });
+
+      it('expects plugins to return object containing command', function(done) {
+        var std = capture();
+        plugin = {
+          name: 'test',
+          match: function() { return { version: 'n' }; }
+        };
+        avn.hooks.after(example('v0.10.26'), { verbose: true }).done(function() {
+          std.restore();
+          expect(std.out).to.be.empty;
+          expect(std.cmd).to.be.empty;
+          expect(std.err).to.eql('avn could not activate node 0.10.26\n' +
+            'error: no plugin passed predicate\n  test: result missing command\n');
+          done();
+        });
+      });
+
+      it('expects plugins to return object containing version', function(done) {
+        var std = capture();
+        plugin = {
+          name: 'test',
+          match: function() { return { command: 'activate n' }; }
+        };
+        avn.hooks.after(example('v0.10.26'), { verbose: true }).done(function() {
+          std.restore();
+          expect(std.out).to.be.empty;
+          expect(std.cmd).to.be.empty;
+          expect(std.err).to.eql('avn could not activate node 0.10.26\n' +
+            'error: no plugin passed predicate\n  test: result missing version\n');
+          done();
+        });
+      });
+
+      it('requires stdcmd stream to work', function(done) {
+        var std = capture(['out', 'err']);
+        avn.hooks.after(example('v0.10.26'))
+        .fail(function(e) {
+          std.restore();
+          expect(e.message).to.eql('Cannot call method \'write\' of undefined');
+          done();
+        })
+        .done();
+      });
+    });
+  });
+});
