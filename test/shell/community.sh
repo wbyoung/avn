645872623627202ebd92fa1ae6f9b4d12d8b00df
Whitney Young <wbyoung@fadingred.com>
2014-05-06 09:34:48 -0700

    Added a placeholder for testing with RVM. Related to #1.

---
+++ test/shell/community.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# TODO: test that community hooks work properly so that `avn` works with `rvm`
+# and other tools. still waiting on a resolution for issue
+# https://github.com/wayneeseguin/rvm/issues/2819 to start working on this.
