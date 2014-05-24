f03c8c10fbc79fe647074223f1579dc2cc915d21
Whitney Young <wbyoung@fadingred.com>
2014-05-24 07:08:43 -0700

    Improved speed.

--- bin/avn.sh
+++ bin/avn.sh
@@ -19,8 +19,8 @@ function __avn_eval() {
 }
 
 function __avn_after_cd() {
-  [[ -f "`pwd`/.node-version" ]] &&
-    __avn_eval after-cd `pwd` "$@" || true
+  [[ -f ".node-version" ]] &&
+    __avn_eval after-cd "$PWD" "$@" || true
 }
 
 export PATH="$HOME/.avn/bin:$PATH"
